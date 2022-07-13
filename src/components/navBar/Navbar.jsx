7ff36513a2a9e588cca5156749348d783e6428c8
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2022-07-13 18:50:07 +0200

    add location 2

--- src/components/navBar/Navbar.jsx
+++ src/components/navBar/Navbar.jsx
@@ -8,53 +8,36 @@ const Navbar = () => {
     <div className="container navBar">
       <img src={logo} alt="" className="navBar__logo" />
 
-    <div className="navBar__tabContent">
-      <div className="navBar__tabContent-searchFields">
-      <div className='inputs'>Location
-      <Select
-      defaultOptionIndex={0}
-      onChange={(data) => console.log(data.value)}
-      options={[
-        {id: 0,
-          label:"South Florida",
-          value:"fffff"
-      },
-      {id: 1,
-          label:"North Florida"
-      },
-      {id: 2,
-          label:"Long Island, New York"
-      },
-      {id: 3,
-          label:"Porto Cala de Medici, Tuscany"
-      },
-      {id: 4,
-          label:"Razo Beach, Galiza"
-      },
-      {id: 5,
-          label:"Norwegian Fjords"
-      },
-      ]}
-      />
+      <div className="navBar__tabContent">
+        <div className="navBar__tabContent-searchFields">
+          <div className="inputs">
+            Location
+            <div className="inputs__selector">
+              <Select
+                defaultOptionIndex={0}
+                onChange={(data) => console.log(data.id)}
+                options={[
+                  { id: "0", label: "Miami - Miami Beach" },
+                  { id: "1", label: "New York - Southampton, Suffolk County" },
+                  { id: "2", label: "Australia - Turquoise Bay" },
+                  { id: "3", label: "Tuscany - Porto Cala de Medici" },
+                  { id: "4", label: "Galiza - Razo Beach" },
+                  { id: "5", label: "Norwegian - Sognefjord, " },
+                ]}
+              />
+            </div>
           </div>
-          <div className='vl' />
-          <div className='inputs'>Check in
-            
-          </div>
-          <div className='vl' />
-          <div className='inputs'>Check out
-            
-          </div>
-          <div className='vl' />
-          <div className='inputs'>Guests
-            
-          </div>
+          <div className="vl" />
+          <div className="inputs">Check in</div>
+          <div className="vl" />
+          <div className="inputs">Check out</div>
+          <div className="vl" />
+          <div className="inputs">Guests</div>
+        </div>
       </div>
-    </div>
 
-      
       <div className="navBar__rigthSide">
-        <div className="navBar__host">Become a Host</div>   
+        <div className="navBar__host">Become a Host</div>
         <ConnectButton />
       </div>
     </div>

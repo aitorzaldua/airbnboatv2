0341de512e15749c90d5106cb99c8bb08ab6434a
Aitor Zaldua <aitor.zaldua@draftdigital.org>
2022-07-14 11:21:54 +0200

    selector finished

--- src/components/navBar/Navbar.jsx
+++ src/components/navBar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./navbar.css";
-import { ConnectButton, Select } from "web3uikit";
+import { ConnectButton, Select, DatePicker, Input, Icon } from "web3uikit";
 import logo from "../../assets/logoAnB.png";
 
 const Navbar = () => {
@@ -18,8 +18,8 @@ const Navbar = () => {
                 onChange={(data) => console.log(data.id)}
                 options={[
                   { id: "0", label: "Miami - Miami Beach" },
-                  { id: "1", label: "New York - Southampton, Suffolk County" },
-                  { id: "2", label: "Australia - Turquoise Bay" },
+                  { id: "1", label: "Bahamas - Paradise Island" },
+                  { id: "2", label: "Australia - Airlie Beach" },
                   { id: "3", label: "Tuscany - Porto Cala de Medici" },
                   { id: "4", label: "Galiza - Razo Beach" },
                   { id: "5", label: "Norwegian - Sognefjord, " },
@@ -28,11 +28,34 @@ const Navbar = () => {
             </div>
           </div>
           <div className="vl" />
-          <div className="inputs">Check in</div>
+          <div className="inputs__date">
+            Check in
+            <DatePicker id="CheckIn" onChange={(event) => console.log(event)} />
+          </div>
           <div className="vl" />
-          <div className="inputs">Check out</div>
+          <div className="inputs__date">
+            Check out
+            <DatePicker
+              id="CheckOut"
+              onChange={(event) => console.log(event)}
+            />
+          </div>
           <div className="vl" />
-          <div className="inputs">Guests</div>
+          <div className="inputs">Guests
+          <Input
+          value={2}
+          name="AddGuests"
+          type="number"
+          omChange={(event) => console.log(Number(event))}
+          />
+          </div>
+          <div className='searchButton'>
+              <Icon
+                fill="#ffffff"
+                size={24}
+                svg="search"
+              />
+            </div>
         </div>
       </div>
 
